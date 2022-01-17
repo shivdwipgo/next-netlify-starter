058e36f5fed8d1643e4bd3bdca43f4c2bf10b526
shivdwipgo <76538503+shivdwipgo@users.noreply.github.com>
2022-01-17 22:11:31 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
       <main>
         <Header title="shivdwip verma" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          shivdwip <code>pages/index.js</code>
         </p>
       </main>
 
